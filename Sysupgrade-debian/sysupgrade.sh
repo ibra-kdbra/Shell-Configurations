2591bcf41070327434388d0ac474f4c2ebf56a11
ibra-kdbra <git-repo-develop@outlook.com>
2026-05-05 23:58:04 +0300

    idk

--- Sysupgrade-debian/sysupgrade.sh
+++ Sysupgrade-debian/sysupgrade.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Description: Simple script to update and upgrade Debian-based systems using nala.
+# Author: ibra-kdbra
+# Dependencies: nala, tput, sudo
+
+set -e
 
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
@@ -6,13 +11,14 @@ reset=$(tput sgr0)
 
 echo "${yellow}==>${reset} nala update..."
 sudo nala update 2>&1
-echo "${green}==>${reset} apt nala finished."
+echo "${green}==>${reset} nala update finished."
 
 echo "${yellow}==>${reset} Running full-upgrade..."
 sudo nala full-upgrade -y 2>&1
 echo "${green}==>${reset} Finished full-upgrade"
 
 echo "${green}==>${reset} Cleaning..."
-sudo apt autoclean -y 2>&1
-sudo apt autoremove -y 2>&1
+sudo nala clean -y 2>&1
+sudo nala autoremove -y 2>&1
 echo "${green}==>${reset} All Updates & Cleanups Finished"
+
